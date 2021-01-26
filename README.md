bf4bc905f73d1313b3a4b819b41350d4bf71a00d
Anthony Trifiletti <anthonytrifiletti@yahoo.com>
2021-01-25 19:18:41 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# JS-code-quiz
+- JavaScript Coding Quiz
+
+    - This is a timed quiz that covers some JavaScript fundamentals.
+
+    - You start the quiz with 120 seconds on the clock.
+
+    - There are four questions.
+        
+        - Each time you answer a question incorrectly, 20 seconds is taken off the timer.
+
+    - When you are finished with the quiz, you will be given a score and an option to put your name in the "Hall of Fame".
+
+        - The "Hall of Fame" is recorded and can be cleared.
+
+        
+
---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include _____.",
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: "Alerts"
+    },
+
+    {
+        title: "Arrays in JavaScript can be used to store _____.",
+        choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
+        answer: "All of the above"
+    },
+
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+        answer: "Parentheses"
+    },
+
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
+        answer: "Quotes"
+    },
+
+
+];
